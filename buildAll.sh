aa76b5f2bb817cfe84e1d2382dfdc79890c041ed
Edward Mezarina <mezarina@us.ibm.com>
2024-02-21 10:17:04 -0500

    LSP4JDT usage

--- buildAll.sh
+++ buildAll.sh
@@ -1,7 +1,7 @@
 set -e
 
 # build LSP4Jakarta JDT Extension
-cd jakarta.jdt && mvn clean; mvn install && cd ..
+cd jakarta.jdt && mvn clean; mvn install -DskipTests && cd ..
 
 # build LSP4Jakarta LS
 cd jakarta.ls && mvn clean; mvn install && cd ..
